294421b2afe822c2f331c3de2f66fe664d4c0933
Brian Janssen <test>
2021-01-14 16:24:27 +0100

    Dunst: update colors

--- user/dunst/.config/dunst/launch.sh
+++ user/dunst/.config/dunst/launch.sh
@@ -5,13 +5,13 @@ pkill dunst
 source "$HOME/.cache/wal/colors.sh"
 
 dunst \
-	-frame_color "${background:-#000000}" \
-	-lfr "${background:-#000000}" \
-	-nfr "${background:-#000000}" \
-	-cfr "${background:-#000000}" \
-	-lb "${color0:-#000000}" \
-	-nb "${color0:-#000000}" \
-	-cb "${color0:-#000000}" \
+	-frame_color "${color0:-#000000}" \
+	-lfr "${color0:-#000000}" \
+	-nfr "${color0:-#000000}" \
+	-cfr "${color0:-#000000}" \
+	-lb "${background:-#000000}" \
+	-nb "${background:-#000000}" \
+	-cb "${background:-#000000}" \
 	-lf "${foreground:-#ffffff}" \
 	-nf "${foreground:-#ffffff}" \
 	-cf "${foreground:-#ffffff}" \
