c6bfad2005a4eba986cf19d39582b4c5dca68652
Brian Janssen <brian-janssen@hotmail.com>
2021-02-15 16:55:40 +0100

    Bitwarden: repalce bwl bwu scripts with rbw and rofi-rbw

--- user/fish/.config/fish/functions/bwl.fish
+++
@@ -1,5 +0,0 @@
-function bwl
-	set -e BW_SESSION
-	echo "" | xclip -selection clipboard -in
-	bw lock
-end
--- user/fish/.config/fish/functions/bwu.fish
+++
@@ -1,5 +0,0 @@
-function bwu
-	if not bw status | rg '"status":"unlocked"' > /dev/null
-		set -xU BW_SESSION (bw unlock --raw)
-	end
-end
