d924cfe8d1cf4d8a92900fa7564a1850ec4d3c6d
Brian Janssen <brian-janssen@hotmail.com>
2021-02-19 19:49:37 +0100

    Spotify: add playerctl usage to simplify code

--- user/polybar/.config/polybar/spotify.sh
+++ user/polybar/.config/polybar/spotify.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -e
-
 case "$1" in
 change | start)
 	echo hook:module/spotify2 >/tmp/polybar_mqueue_top
 	;;
 stop)
+	# pause also implies stop, so let's ignore that
+	;;
+close)
 	echo hook:module/spotify1 >/tmp/polybar_mqueue_top
 	;;
 esac
